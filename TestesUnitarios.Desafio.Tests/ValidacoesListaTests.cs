fd3ec7330964b890073cba1ce1aabb952df62726
Eduarda Ventura <113445954+menv25@users.noreply.github.com>
2024-01-16 20:19:59 -0300

    Update ValidacoesListaTests.cs

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 namespace TestesUnitarios.Desafio.Tests
 {
-    }
     public class ValidacoesListaTests
     {
         private ValidacoesLista _validacoes = new ValidacoesLista();
